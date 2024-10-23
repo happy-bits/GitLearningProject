6519a564720c88ccebc21cdcd07f0e592fdac82a
Oscar Olsson <happybits@gmail.com>
2024-10-23 19:54:56 +0200

    Added exclamation mark

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
 
 Console.WriteLine("Hello, World!");
-Console.WriteLine("I live in Sweden");
-Console.WriteLine("I like pandas");
+Console.WriteLine("I live in Sweden!");
+Console.WriteLine("I like pandas!");
 
 string text = System.IO.File.ReadAllText("panda-info.txt");
 System.Console.WriteLine($"Here in info from the file 👉 {text}");
